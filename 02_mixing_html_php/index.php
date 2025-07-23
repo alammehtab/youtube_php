a0750c4e58f4c914abfaa23914f480e8817a5bcf
alammehtab <mehtaba728@gmail.com>
2025-07-23 18:19:56 +0500

    add 02_mixing_html_php

---
+++ 02_mixing_html_php/index.php
@@ -0,0 +1,18 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alkoders</title>
+</head>
+<body>
+    <h1>My PHP Website</h1>
+    <?php
+        echo "<h1 align='center'>Hello World!</h1>";
+    ?>
+</body>
+</html> -->
+
+<?php
+    echo "Helloo inside the terminal";
+?>
